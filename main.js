e6337cc7884ba6282675a72f8d5098b34dbe732c
Mariia <mytsko.mariia@gmail.com>
2020-08-16 23:16:20 +0200

    advanced task

--- main.js
+++ main.js
@@ -7,15 +7,31 @@ box.style.flexWrap = "wrap";
 const wrapper = document.getElementById("wrapper");
 wrapper.append(box);
 
-const createBlocks = () => {
+const randomColor = () => {
+  return `#` + Math.floor(Math.random() * Math.pow(255, 3)).toString(16);
+};
+
+const generateBlocks = () => {
+  for (let i = 0; i < 25; i++) {
+    const b = document.createElement(`div`);
+    b.style.width = `50px`;
+    b.style.height = `50px`;
+    b.style.backgroundColor = randomColor();
+    box.append(b);
+  }
+};
+
+const generateBlockInterval = () => {
   for (let i = 0; i < 25; i++) {
     const b = document.createElement(`div`);
 
     b.style.width = `50px`;
     b.style.height = `50px`;
-    b.style.backgroundColor =
-      `#` + Math.floor(Math.random() * Math.pow(255, 3)).toString(16);
+    setInterval(() => {
+      b.style.backgroundColor = randomColor();
+    }, 1000);
+
     box.append(b);
   }
 };
-createBlocks();
+generateBlockInterval();
